9a2ad7556169460cd1d8e5ae346617d125dc19c0
MJHugo <108774643+7BlackJack@users.noreply.github.com>
2023-10-13 13:14:44 +0800

    update all Adapter port

--- weibo_project/api/urls.py
+++ weibo_project/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('search_list/<str:query>/', views.search_list),
     path('rank_history/<str:text>/', views.rank_history),
-    # 为其他视图函数也添加路由
+    path('time_id/<str:timestamp>/', views.time_id),
+    path('time_id_by_name/<str:name>/', views.time_id_by_name),
+    path('all_data_list/<str:time_id>/', views.all_data_list),
 ]
--- weibo_project/api/views.py
+++ weibo_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..weibo_api.weibo_api import WeiboAPI # 引入你的WeiboAPI类
 
+
 @api_view(['GET'])
 def search_list(request, query):
     api = WeiboAPI()
@@ -12,7 +13,23 @@ def search_list(request, query):
 @api_view(['GET'])
 def rank_history(request, text):
     api = WeiboAPI()
-    result = api.get_rank_history(text)
+    result = api.get_getrankhistory_name(text)
     return Response(result, status=status.HTTP_200_OK)
 
-# 你可以添加更多的视图函数，用于调用WeiboAPI的其他方法
+@api_view(['GET'])
+def time_id(request, timestamp):
+    api = WeiboAPI()
+    result = api.get_time_id(timestamp)
+    return Response(result, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def time_id_by_name(request, name):
+    api = WeiboAPI()
+    result = api.get_timeid_byname(name)
+    return Response(result, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def all_data_list(request, time_id):
+    api = WeiboAPI()
+    result = api.get_all_data_list(time_id)
+    return Response(result, status=status.HTTP_200_OK)
